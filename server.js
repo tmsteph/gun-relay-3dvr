1ff13194d275cd285f357b3c39336853a8e51f52
Thomas Stephens <tmsteph1290@gmail.com>
2025-06-22 14:28:48 -0700

    Update server.js add defulte route

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
+
+// Health check or homepage
+app.get('/', (req, res) => res.send('🟢 Gun relay is running'));
+
+// Serve Gun's static files + WebSocket handler
 app.use(Gun.serve);
 
 const server = app.listen(port, '0.0.0.0', () => {
